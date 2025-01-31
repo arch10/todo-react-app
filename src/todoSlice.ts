29f39a94f07814649f190539f11676cd049e0822
Archit Raj <arch1824@gmail.com>
2025-01-31 14:28:28 +0530

    Create todoSlice.ts

---
+++ src/todoSlice.ts
@@ -0,0 +1,12 @@
+// ## ADD your todo slice here
+
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { Todo } from "./type";
+
+// export type TodoState = {
+//   todos: Todo[];
+// };
+
+// const initialState: TodoState = {
+//   todos: [],
+// };
